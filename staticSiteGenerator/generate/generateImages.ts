4014b640eec89c4c4b8dc8d0d2eab68624186149
Senad Redzic <senad.redzic@nc-vision.com>
2025-02-16 12:18:04 +0100

    fixing images. svg and ico should be excluded

--- staticSiteGenerator/generate/generateImages.ts
+++ staticSiteGenerator/generate/generateImages.ts
@@ -39,55 +39,39 @@ const processPath = async (
   }[]
 > => {
   const imagePaths = await glob([
-    `${source}/**/*.{jpg,jpeg,png,gif,webp}`,
-    `${source}/**/*.{svg,ico}`,
+    `${source}/**/*.{jpg,jpeg,png,gif,webp,svg,ico}`,
   ]);
 
   const targetPath = path.join(ctx.outputDirectory, "static", "images");
   await fs.ensureDir(targetPath);
 
-  const generateImage = async (
-    imagePath: string,
-  ): Promise<{ key: string; srcHash: string; srcsetHash: string }> => {
+  const generateImage = async (imagePath: string) => {
     const ext = path.extname(imagePath).toLowerCase();
-
     if (ext === ".svg" || ext === ".ico") {
       const hash = await generateHashFromFile(imagePath);
-      const target = path.join(targetPath, `${hash}${ext}`);
-      await fs.copyFile(imagePath, target);
+      const fileName = `${hash}${ext}`;
+      await fs.copyFile(imagePath, path.join(targetPath, fileName));
+      const staticPath = `/static/images/${fileName}`;
       return {
         key: path.relative(source, imagePath),
-        srcHash: `/static/images/${hash}${ext}`,
-        srcsetHash: `/static/images/${hash}${ext}`,
-      };
-    } else {
-      const img1 = await optimizeImage(imagePath, 320, targetPath);
-      const img2 = await optimizeImage(imagePath, 640, targetPath);
-      const img3 = await optimizeImage(imagePath, 1280, targetPath);
-
-      return {
-        key: path.relative(source, imagePath),
-        srcHash: `/static/images/${img1}.webp`,
-        srcsetHash: `/static/images/${img1}.webp 320w, /static/images/${img2}.webp 640w, /static/images/${img3}.webp 1280w`,
+        srcHash: staticPath,
+        srcsetHash: staticPath,
       };
     }
+
+    const img1 = await optimizeImage(imagePath, 320, targetPath);
+    const img2 = await optimizeImage(imagePath, 640, targetPath);
+    const img3 = await optimizeImage(imagePath, 1280, targetPath);
+
+    return {
+      key: path.relative(source, imagePath),
+      srcHash: `/static/images/${img1}.webp`,
+      srcsetHash: `/static/images/${img1}.webp 320w, /static/images/${img2}.webp 640w, /static/images/${img3}.webp 1280w`,
+    };
   };
 
-  const generated = await Promise.all(
-    imagePaths.map(async (imagePath) => {
-      try {
-        return await generateImage(imagePath);
-      } catch (error) {
-        console.error(
-          `Skipping image ${imagePath} due to error:`,
-          error.message,
-        );
-        return null;
-      }
-    }),
-  );
-
-  return generated.filter(Boolean);
+  const generated = await Promise.all(imagePaths.map(generateImage));
+  return generated;
 };
 
 export const generateImages = async (
